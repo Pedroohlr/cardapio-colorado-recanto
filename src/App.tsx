c88705615020e0ebfe718430751663518d698067
Pedrinho Hilário <flygames1214@gmail.com>
2025-05-28 13:40:21 -0300

    instalando componentes da shadcn e criando loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import { Button } from "./components/ui/button"
+import { Loading } from "./components/Loading";
+import { useCategories } from "./hooks/useCategories"
+import { 
+  Carousel, 
+  CarouselContent, 
+  CarouselItem 
+  } from "./components/ui/carousel";
 
 function App() {
+  const { categories, loading } = useCategories();
+  
+  if (loading) return <Loading />
+
   return (
-    <div className="bg-black">
-      <Button>
-        Olá
-      </Button>
-    </div>
+    <main>
+      <div>
+        <Carousel 
+          opts={{
+            align: "start"
+          }}
+        >
+          <CarouselContent>
+            {categories.map((cat) => (
+              <CarouselItem>
+                <img 
+                  src={cat.foto!}
+                  className="rounded-full"
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>  
+      </div>    
+    </main>
   )
 }
 
---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "./ui/skeleton";
+
+export const Loading = () => {
+    return(
+        <Skeleton className="h-12 w-12 rounded-full" />
+    )
+}
